b4c99390afa61b1188dc6c75ef2a15b309b099df
mbcod3 <58457269+mbcod3@users.noreply.github.com>
2020-01-23 10:35:45 +0500

    init

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from './logo'
+
+export default function Nav() {
+
+  const navLinks = ['features', 'pricing','resources','login','sign up']
+  return (
+    <nav>
+      <img src={logo} alt="logo"/>
+      <div>
+        
+      </div>
+    </nav>
+  )
+}
