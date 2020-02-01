7375459b3fba8efbc3bccce4c20bf3dbdcbd7e74
mbcod3 <58457269+mbcod3@users.noreply.github.com>
2020-02-01 08:53:07 +0500

    Updates

---
+++ precache-manifest.cf6b2c875ffb187babdc0893ed6b0159.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c730fb6e2ed861015c6647da7220da19",
+    "url": "/url-shortener/index.html"
+  },
+  {
+    "revision": "efc07c4930b5808238e9",
+    "url": "/url-shortener/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6d859bf07564c045f1d7",
+    "url": "/url-shortener/static/js/2.6b48dd37.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/url-shortener/static/js/2.6b48dd37.chunk.js.LICENSE"
+  },
+  {
+    "revision": "efc07c4930b5808238e9",
+    "url": "/url-shortener/static/js/main.6a762a7b.chunk.js"
+  },
+  {
+    "revision": "41cc0be1bff219ece9d2",
+    "url": "/url-shortener/static/js/runtime-main.03227aec.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/url-shortener/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/url-shortener/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/url-shortener/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/url-shortener/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/url-shortener/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/url-shortener/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/url-shortener/static/media/logo.6387840f.svg"
+  }
+]);
